7d18fc3171f31d725d2c17b8896a753ba770f5ea
blupandaman <blupandaman@pm.me>
2024-06-11 16:10:30 -0400

    style: some formatting changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
-import Image from "next/image";
-import imageTapioca from "./tapioca.png";
-import Link from "next/link";
 import { AnimatedNumber } from "./animated-number";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-start justify-between bg-[#0a0949] p-8 text-center text-black">
-      <Link
-        className="underline underline-offset-4"
-        href="https://www.tapioca.xyz/"
-        target="_blank"
-      >
-        <Image src={imageTapioca} alt="Tapioca SVG" className="h-5 w-auto" />
-      </Link>
-      <div className="w-full items-center justify-center space-y-8 p-0 font-digital text-4xl font-bold  text-[#faafe7] sm:p-24 md:text-6xl">
+      <div />
+
+      <div className="w-full items-center justify-center space-y-8 p-0 font-digital text-6xl  font-bold text-[#faafe7] md:text-8xl">
+        <p className="pb-6 text-lg font-semibold tracking-wider">
+          tapioca genesis lbp
+        </p>
+
         <AnimatedNumber />
 
         <p className="text-lg font-semibold tracking-wider">
@@ -22,22 +18,10 @@ export default function Home() {
       </div>
 
       <div className="mx-auto font-sans font-semibold text-[#faafe7]">
-        Made by{" "}
-        <Link
-          className="underline underline-offset-4"
-          href="https://twitter.com/blupandaman"
-          target="_blank"
-        >
-          blupandaman
-        </Link>{" "}
-        💙🐼 in support for{" "}
-        <Link
-          className="underline underline-offset-4"
-          href="https://www.tapioca.xyz/"
-          target="_blank"
-        >
-          tapioca.xyz
-        </Link>
+        Real Time LBP stats brought to you by{" "}
+        <span className="underline underline-offset-4">
+          Capy Data Analytics
+        </span>
       </div>
     </main>
   );
