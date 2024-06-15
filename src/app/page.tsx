ccdbe6ef67a476ef657b39e06d83632d2a3c4676
Weston <dogooddowell@protonmail.com>
2024-06-15 12:27:57 -0500

    chore: update info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
       <div />
 
       <div className="w-full items-center justify-center space-y-8 p-0 font-digital text-6xl  font-bold text-[#faafe7] md:text-7xl">
-        <p className="text-lg font-semibold tracking-wider">
-         Tapioca Airdrop Options
-        </p>
+      <p className="text-lg md:text-8xl font-semibold tracking-wider text-white font-sans">
+      Tapioca Airdrop Options
+      </p>
 
         <div className="flex w-full flex-col justify-center gap-8 sm:flex-row sm:gap-16">
           <div className="space-y-4">
@@ -20,19 +20,18 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="mx-auto font-sans font-semibold text-[#faafe7]">
-      View the live Airdrop Options contract on {" "}
-      <a href="https://arbiscan.io/address/0xcf2e155eaa30706ff245679838006e6e092dd8b5" className="underline underline-offset-4" target="_blank" rel="noopener noreferrer">
-      Arbiscan
-      </a>
-      </div>
+      <div className="mx-auto font-sans font-semibold text-[#faafe7] space-y-4">
+  View the live Airdrop Options contract on {" "}
+  <a href="https://arbiscan.io/address/0xcf2e155eaa30706ff245679838006e6e092dd8b5" className="underline underline-offset-4" target="_blank" rel="noopener noreferrer">
+    Arbiscan
+  </a>
+  <hr className="border-t border-gray-200" />
+  <p>Real-Time analytics brought to you by{" "}</p>
+  <span className="underline underline-offset-4">
+  Capy Data Analytics
+  </span>
+  </div>
 
-      <div className="mx-auto font-sans font-semibold text-[#faafe7]">
-        Real Time LBP stats brought to you by{" "}
-        <span className="underline underline-offset-4">
-          Capy Data Analytics
-        </span>
-      </div>
-    </main>
+</main>
   );
 }
